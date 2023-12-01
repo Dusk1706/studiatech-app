e180f0d5f62812e00f3ae3d13ec9e2f1ca03e750
Danifaleyy <124246616+Danifaleyy@users.noreply.github.com>
2023-12-01 02:22:02 -0700

    Profile implemented
    implementacion del profil

--- studiatech/src/pages/_app.jsx
+++ studiatech/src/pages/_app.jsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-
-import Head from "next/head";
 import { StateProvider } from "@/context/StateContext";
 import reducer, { initialState } from "@/context/StateReducers";
 import '@/styles/globals.css'
+import Head from "next/head";
+import { useRouter } from "next/router";
+
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
       <Head>
@@ -15,11 +17,12 @@ export default function App({ Component, pageProps }) {
       </Head>
       <div className="relative flex flex-col h-screen justify-between">
         <Navbar />
-        <div className={'mb-auto w-full mx-auto'}>
-          <Component {...pageProps} />;
+        <div className={'mb-auto w-full mx-auto ${router.pathname !== "/" ? "mt-36" : ""}'}
+        >
+          <Component{...pageProps} />;
         </div>
-      <Footer />
-      </div>
-    </StateProvider>
-  )
+        <Footer />
+        </div>
+        </StateProvider>
+  );
 }
